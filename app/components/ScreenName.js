23e3c7d6b52864a263e3ab21d00918a6779d7d18
Praveen Kumar <praveen@unbxd.com>
2016-08-06 16:39:30 +0530

    components/ScreenName.js - using error component inside screen name component

--- app/components/ScreenName.js
+++ app/components/ScreenName.js
@@ -1,8 +1,13 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var Container = require('./Container');
+var ErrorBlock = require('./ErrorBlock')
 
 function ScreenName (props) {
+  let error = '';
+  if(props.error){
+    error = <ErrorBlock error={props.error} ></ErrorBlock>
+  }
   return (
     <Container>
       <h1>{props.header}</h1>
@@ -16,6 +21,7 @@ function ScreenName (props) {
               type='text'
               value={props.username} />
           </div>
+          {error}
           <div className="form-group col-sm-4 col-sm-offset-4">
             <button
               className="btn btn-block btn-success"
